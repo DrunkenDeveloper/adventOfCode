65b8f33e19537a686c71a6400f1d6b673b5c74ac
DrunkenDeveloper <43037476+DrunkenDeveloper@users.noreply.github.com>
2023-12-01 14:54:04 +0100

    Add files via upload

---
+++ 1-1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Advent of Code 1-1");
+
+
+var lines = File.ReadLines("input1-1.txt");
+Console.WriteLine(lines.Select(l => Regex.Matches(l, @"\d")).Select(mc => string.Join(string.Empty, [mc[0].Value, mc[^1].Value])).Select(c => int.Parse(c)).Sum());
